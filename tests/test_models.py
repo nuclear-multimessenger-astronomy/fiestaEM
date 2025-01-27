7e3a63ca61bb36cce77f5ca7907ff63a136cb4b0
haukekoehn <hkoehn@urash.gw.physik.uni-potsdam.de>
2025-01-27 18:53:16 +0100

    Updated tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -10,12 +10,14 @@ from fiesta.inference.lightcurve_model import AfterglowFlux
 working_dir = os.path.dirname(__file__)
 model_dir = os.path.join(working_dir, "models")
 
-model = AfterglowFlux(name="flux",
-                      directory=model_dir,
+def test_models():
+
+    model = AfterglowFlux(name="flux",
+                          directory=model_dir,
                       filters=["radio-6GHz", "bessellv", "X-ray-1keV"],
                       model_type="MLP")
 
-X = [3.141/30, 54., 0.05, -1., 2.5, -2., -4.]
-mag = model.predict(dict(zip(model.parameter_names, X)))
+    X = [3.141/30, 54., 0.05, -1., 2.5, -2., -4.]
+    mag = model.predict_abs_mag(dict(zip(model.parameter_names, X)))
 
 # TODO: Add more model types here
