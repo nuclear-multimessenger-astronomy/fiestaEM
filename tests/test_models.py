520f4140590d79315e531c9f92bd4d02efd9b598
haukekoehn <hkoehn@ninurta.gw.physik.uni-potsdam.de>
2025-05-12 13:46:45 +0200

    Unit tests for all ML architectures

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,6 +1,6 @@
 import os
 
-from fiesta.inference.lightcurve_model import AfterglowFlux
+from fiesta.inference.lightcurve_model import AfterglowFlux, BullaLightcurveModel
 
 
 ##############
@@ -9,14 +9,37 @@ from fiesta.inference.lightcurve_model import AfterglowFlux
 
 working_dir = os.path.dirname(__file__)
 model_dir = os.path.join(working_dir, "models")
+FILTERS = ["radio-6GHz", "bessellv", "X-ray-1keV"]
 
-def test_models():
+def test_MLP():
 
-    model = AfterglowFlux(name="flux",
-                          directory=model_dir,
-                          filters=["radio-6GHz", "bessellv", "X-ray-1keV"])
+    model = AfterglowFlux(name="pbag_gaussian_MLP", 
+                          filters=FILTERS)
+    
+    X = [3.141/30, 54., 0.05, 2., -1., 2.5, -2., -4., 500]
+    params = dict(zip(model.parameter_names, X))
+    params["luminosity_distance"] = 40.0
+    params["redshift"] = 0.0
+    mag = model.predict(params)
 
-    X = [3.141/30, 54., 0.05, -1., 2.5, -2., -4.]
-    mag = model.predict_abs_mag(dict(zip(model.parameter_names, X)))
+def test_CVAE():
 
-# TODO: Add more model types here
+    model = AfterglowFlux(name="pbag_gaussian_CVAE", 
+                          filters=FILTERS)
+    
+    X = [3.141/30, 54., 0.05, 2., -1., 2.5, -2., -4., 500]
+    params = dict(zip(model.parameter_names, X))
+    params["luminosity_distance"] = 40.0
+    params["redshift"] = 0.0
+    mag = model.predict(params)
+
+def test_LC():
+
+    model = BullaLightcurveModel(name="Bu2025",
+                                 filters=["besselli", "bessellg", "bessellr"])
+    
+    X = [120, -2, 0.2, 0.3, -1.5, 0.4, 0.3, 0.1]
+    params = dict(zip(model.parameter_names, X))
+    params["luminosity_distance"] = 40.0
+    params["redshift"] = 0.0
+    mag = model.predict(params)
