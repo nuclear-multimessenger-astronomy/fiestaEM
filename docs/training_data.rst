aedd05d4d2dbcc066e84baf803e2c6970590021e
haukekoehn <hkoehn@kingu.gw.physik.uni-potsdam.de>
2025-12-09 17:03:50 +0100

    Second commit for training update

--- docs/training_data.rst
+++ docs/training_data.rst
@@ -1,9 +1,9 @@
 Training data
 -------------
 
-In fiesta, training a surrogate starts from a ``.hdf5`` file that contains all the training, validation, and test data, plus the meta data of the model.
+In fiesta, training a surrogate starts from an ``.hdf5`` file that contains all the training, validation, and test data, plus the meta data of the model.
 This ``.hdf5`` file needs to follow a certain format, although how exactly it is created does not matter. 
-There are some helper functions in ``fiesta`` that can create training data from GRB afterglow models () or summarize the output from ``possis`` and ``gwemopt`` into a ``.hdf5`` file ().
+There are some helper functions in ``fiesta`` that can create training data from GRB afterglow models (``fiesta.train.AfterglowData``) or summarize the output from ``possis`` and ``gwemopt`` into a ``.hdf5`` file (``fiesta.utils.convert_POSSIS_outputs_to_h5``).
 
 Data file format
 ^^^^^^^^^^^^^^^^
@@ -20,7 +20,7 @@ Further, the file needs the following groups that contain the actual training da
 Each of these groups contains exactly two sets. 
 One set has to be named ``X`` and contains the parameters as a vector of shape ``(n_samples, n_params)``. They need to match to the parameters provided in the ``parameter_names`` set.
 The other set has to be named ``y`` and contains the a vector of shape ``(n_samples, n_nus, n_times)`` where the last two shapes are the length of the ``nu`` and ``time`` array.
-The entries are the corresponding flux densities at 10 pc (but zero redshift, i.e. source frame) in $log_{10}(mJys) = log{10}_(1e-26 erg / (s Hz cm^2))$.
+The entries are the corresponding flux densities at 10 pc (but zero redshift, i.e. source frame) in $log_{10}(\\mathrm{mJys}) = log_{10}(\\mathrm{1e-26 erg / (s Hz cm^2)})$.
 
 Optionally, there can be a data group ``special_train``. This group can contain further groups, labeled arbitrarily, that contain ``X`` and ``y`` data sets, that can supplement the larger training data set in ``train``, should certain areas of the parameter space need extra coverage.
 They can be used during training through the ``DataManager`` interface.
--- examples/training/training_CVAE.py
+++ examples/training/training_CVAE.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import h5py
 
-from fiesta.train.FluxTrainer import PCATrainer
+from fiesta.train.FluxTrainer import CVAETrainer
 from fiesta.inference.lightcurve_model import FluxModel
 from fiesta.train.neuralnets import NeuralnetConfig
 
@@ -10,25 +10,25 @@ from fiesta.train.neuralnets import NeuralnetConfig
 ### SETUP ###
 #############
 
-tmin = 0.21 # days
-tmax = 21
+tmin = 1e-4 # days
+tmax = 1e3
 
 
-numin = 1e14 # Hz 
-numax = 2e15
+numin = 1e9 # Hz 
+numax = 1e18
 
-n_training = 240 
-n_val = 30
+n_training = 200
+n_val = 10
 
-n_pca = 30
+image_size = np.array([42, 57])
 
-name = "gwemopt_MLP"
+name = "test_MLP"
 outdir = f"./model/"
-file = "./gwemopt_raw_data.h5"
+file = "./data/afterglowpy_tophat_reduced_set.h5"
 
-config = NeuralnetConfig(output_size=n_pca,
-                         nb_epochs=100_000,
-                         hidden_layer_sizes = [32, 32],
+config = NeuralnetConfig(output_size= int(np.prod(image_size)),
+                         nb_epochs=10_000,
+                         hidden_layer_sizes = [200, 100],
                          learning_rate =2e-4)
 
 
@@ -38,19 +38,21 @@ config = NeuralnetConfig(output_size=n_pca,
 
 
 data_manager_args = dict(file = file,
-                           n_training= n_training, 
-                           n_val= n_val, 
-                           tmin= tmin,
-                           tmax= tmax,
-                           numin = numin,
-                           numax = numax, 
-                           )
+                         n_training= n_training, 
+                         n_val= n_val, 
+                         tmin= tmin,
+                         tmax= tmax,
+                         numin = numin,
+                         numax = numax,
+                         special_training=["special_1"],
+                         )
 
-trainer = PCATrainer(name,
+
+trainer = CVAETrainer(name,
                      outdir,
                      data_manager_args = data_manager_args,
                      plots_dir=f"./benchmarks/",
-                     n_pca=n_pca,
+                     image_size=image_size,
                      save_preprocessed_data=False
                      )
 
